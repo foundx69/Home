4f36dfa8be79cc4a227ed89f60e12ca7f34fb68e
Task1 <hs.mikhail@gmail.com>
2022-10-23 15:06:45 +0300

    Task1

---
+++ src/Task1/Main.java
@@ -0,0 +1,30 @@
+package Task1;
+
+public class Main {
+    int a = 0; //глобальная переменная
+    public static void main(String[] args) {
+
+        //объект
+        Person misha = new Person();
+
+        //типы переменных
+        byte num = 0;
+        short num2 = 10000;
+        int num3 = 100000;
+        long num4 = -1;
+
+        float num5 = 24.67f;
+        double num6 = 3456.678865567d;
+
+        char sym = 'B';
+
+        boolean ItsTrue = true;
+
+        String str = "Hello, Mikhail!";
+
+        System.out.println(str);
+    }
+    public void method(){
+        int b = 1; //локальная переменная
+    }
+}
---
+++ src/Task1/Person.java
@@ -0,0 +1,17 @@
+package Task1;
+
+public class Person {
+    private String name = "Misha";
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+
+
